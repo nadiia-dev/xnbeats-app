9b5183c103ee4a92b55243abeda8d0eb71b6e7ac
nadiia-dev <nadiia.pavliuchenko.dev@gmail.com>
2025-04-28 16:41:00 +0300

    add main navigation && header and footer

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <nav className="site-header py-1 sticky-top">
+        <div className="container d-flex flex-column flex-md-row justify-content-between">
+          <Link to="/" className="logo" aria-label="Product">
+            XN<span>B</span>
+          </Link>
+          <Link className="mt-4" to="/">
+            Home
+          </Link>
+          <Link className="mt-4" to="/contact">
+            Contact
+          </Link>
+          <Link className="mt-4" to="/login">
+            Login
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
